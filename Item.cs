55713863110c68d5b9f5a3a213b0f2b0674cee7a
Grumpylama <65866382+Grumpylama@users.noreply.github.com>
2022-07-06 23:47:06 +0200

    Add project files.

---
+++ Item.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CBoard
+{
+    public class Item
+    {
+        public double price { get; private set; }
+        public string name { get; private set; }
+        private string imgPath;
+
+        public Item (string name, double price)
+        {
+            this.price = price;
+            this.name = name;
+        }
+
+        public void addImg(string imgPath)
+        {
+            this.imgPath = imgPath;
+        }
+    }
+}
---
+++ Recciept.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CBoard
+{
+    class Recciept
+    {
+        private List<Item> items;
+
+        public Recciept()
+        {
+            items = new List<Item>();
+        }
+        public void addItem(Item i)
+        {
+            items.Add(i);
+        }
+
+        public void removeitem(Item)
+        {
+            items.
+        }
+    }
+}
